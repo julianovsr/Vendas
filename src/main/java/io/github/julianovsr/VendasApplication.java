5a7e19431994ce4bd4598808385d9715eb74fe62
Juliano Victor Silva Ramos <juliano.ramos@equatorialenergia.com.br>
2021-04-18 23:09:59 -0300

    Config SpringBoot

--- src/main/java/io/github/julianovsr/VendasApplication.java
+++ src/main/java/io/github/julianovsr/VendasApplication.java
@@ -2,11 +2,21 @@ package io.github.julianovsr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class VendasApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
+
     }
+
+    @GetMapping("/hello")
+    public String helloWorld(){
+        return "Hello World!";
+    }
+
 }
