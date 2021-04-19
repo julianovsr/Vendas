bf79b43d7db683289ec9ec00bd277dd5ec459365
Juliano Victor Silva Ramos <juliano.ramos@equatorialenergia.com.br>
2021-04-18 23:03:11 -0300

    Config SpringBoot

--- src/main/java/io/github/julianovsr/VendasApplication.java
+++ src/main/java/io/github/julianovsr/VendasApplication.java
@@ -1,12 +1,12 @@
 package io.github.julianovsr;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class VendasApplication {
 
-
-    public public static void main(String[] args) {
-
+    public static void main(String[] args) {
+        SpringApplication.run(VendasApplication.class, args);
     }
 }
