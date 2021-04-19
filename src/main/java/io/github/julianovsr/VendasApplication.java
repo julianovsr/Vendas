505357bf3ed05fc426f8b9e0bf34405ee23a29eb
Juliano Victor Silva Ramos <juliano.ramos@equatorialenergia.com.br>
2021-04-18 22:44:56 -0300

    inital commit

---
+++ src/main/java/io/github/julianovsr/VendasApplication.java
@@ -0,0 +1,12 @@
+package io.github.julianovsr;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendasApplication {
+
+
+    public public static void main(String[] args) {
+
+    }
+}
